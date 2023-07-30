54ac2a4de0db6d81c6d36e12ef6065f39ae0d8f6
xiomara13 <jamara0917@gmail.com>
2023-07-30 12:17:51 -0700

    print

--- _printf.c
+++ _printf.c
@@ -4,42 +4,35 @@
 #include "main.h"
 int _printf(const char *format, ...)
 {
-	    va_list ar;
-	        va_start(ar, format);
-
-		    int cha = 0;
-
-
-		        while (*format)
-				    {
-					            if (*format == '%')
-							            {
-									                format++;
-											            if (*format == '%')
-													                { 
-																                putchar('%');
-																		                cha++;
-																				            
-																				            }
-												                else 
-															            {
-																	                    int (*oper_fun)(va_list) = structf(*format);
-																			                    if (oper_fun != NULL)
-																						                    {
-																									                        cha += oper_fun(ar);
-																												                }
-																					                }
-														        }
-
-						            else
-								            {
-										                putchar(*format);
-												            cha++;
-													            }
-
-							            format++;
-								        }
-
-			    va_end(ar);
-			        return cha;
+	va_list ar;
+	va_start(ar, format);
+	int cha = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				putchar('%');
+				cha++;
+			}
+			else
+			{
+				int (*oper_fun)(va_list) = structf(*format);
+				if (oper_fun != NULL)
+				{
+					cha += oper_fun(ar);
+				}
+			}
+		}
+		else
+		{
+			putchar(*format);
+			cha++;
+		}
+		format++;
+	}
+	va_end(ar);
+	return cha;
 }
